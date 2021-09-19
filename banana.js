7d817a1646be979192c85b5e647179b897489706
Rajitha Muppalla Penchala <mprajitha041098@gmail.com>
2021-09-19 12:46:23 +0530

    input and output

--- banana.js
+++ banana.js
@@ -1,2 +1,17 @@
-var reference=document.querySelector("#btn")
-console.log(reference)
+var button=document.querySelector("#button");
+var textarea=document.querySelector("#textarea")
+var output=document.querySelector("#output")
+
+function clickhandler(){
+    console.log("clicked")
+    console.log("input",textarea.value)
+    output.innerText="abcdefgh"+textarea.value;
+    
+    
+}
+
+button.addEventListener("click",clickhandler)
+
+
+
+
