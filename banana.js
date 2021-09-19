07662f543253ab36865629397d5af68a955ceac7
Rajitha Muppalla Penchala <mprajitha041098@gmail.com>
2021-09-19 16:00:50 +0530

    wired to server

--- banana.js
+++ banana.js
@@ -1,17 +1,28 @@
 var button=document.querySelector("#button");
 var textarea=document.querySelector("#textarea")
 var output=document.querySelector("#output")
-
-function clickhandler(){
-    console.log("clicked")
-    console.log("input",textarea.value)
-    output.innerText="abcdefgh"+textarea.value;
-    
-    
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function translationurl(text){
+return url+"?"+"text="+text;
 }
-
+function errorhandler(error)
+{
+    console.log("error")
+    alert("something wrong with the server try again")
+}
+function clickhandler(){
+    console.log("clicked");
+    var input=textarea.value;
+    fetch(translationurl(input))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        output.innerText=translatedText;
+     })
+    .catch(errorhandler)
+    output.innerText("hi")
+}
 button.addEventListener("click",clickhandler)
-
 
 
 
