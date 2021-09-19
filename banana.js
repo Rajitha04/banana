c8468f91b32dfd2d7ff36e0bc411eea71e834804
Rajitha Muppalla Penchala <mprajitha041098@gmail.com>
2021-09-19 21:57:23 +0530

    final done

--- banana.js
+++ banana.js
@@ -1,28 +1,30 @@
 var button=document.querySelector("#button");
 var textarea=document.querySelector("#textarea")
 var output=document.querySelector("#output")
-//var url="https://api.funtranslations.com/translate/minion.json";
-var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url="https://api.funtranslations.com/translate/minion.json";
+
+
 function translationurl(text){
 return url+"?"+"text="+text;
+}
+function clickhandler(){
+    console.log("clicked");
+    var input=textarea.value;
+   var user= fetch(translationurl(input))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        output.innerText=translatedText;
+        
+     })
+    .catch(errorhandler)
+
 }
 function errorhandler(error)
 {
     console.log("error")
     alert("some error occured try again later!!")
-}
-function clickhandler(){
-    console.log("clicked");
-    var input=textarea.value;
-    fetch(translationurl(input))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText=json.contents.translated;
-output.innerText=translatedText;
-        
-     })
-    .catch(errorhandler)
-
+    
 }
 button.addEventListener("click",clickhandler)
 
